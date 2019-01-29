95db709c06621aaab85950a70999098d67853104
bartmate <bartmate@gmail.com>
2019-01-29 14:18:33 +0100

    MCTS implementation...

--- params.py
+++ params.py
@@ -1,1 +1,11 @@
-MTCS_NR = 256
+#Nr. of Simulation in MTCS
+MTCS_NR   = 256     
+
+#Temperature for controlling exploration
+MTCS_TAU  = 0.1     
+
+#Competitieve play: Chosing the node with maximal N (equivalent to TAU = infinitesimally small)
+MTCS_COMP = False   
+
+#Coefficient for U in Q+U
+MTCS_U_COEFF = 2.0
