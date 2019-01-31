6b6c2a59f8088d32e443f43b5b46c432f46ab152
Máté Bartalos <bartmate@gmail.com>
2019-01-31 23:03:55 +0100

    Developing & Testing Model

--- Game.py
+++ Game.py
@@ -40,19 +40,24 @@ class Game:
         self.winner = 0
         self.next = 3-self.next
         
-    def fill_grids_for_nn(gridx, grido, gridnext, gridlast):
+    def fill_grids_for_nn(self,nn_input): 
+        #nn_input: Input for NN, containing one position (shape: (1,15,15,4))
         for x in range(15):
             for y in range(15):
-                gridx[x,y] = 0
+                #grid-x
+                nn_input[0,x,y,0] = 0
                 if self.grid[x,y]==1:
-                    gridx[x,y] = 1
-                grido[x,y] = 0
+                    nn_input[0,x,y,0] = 1
+                #grid-o
+                nn_input[0,x,y,1] = 0
                 if self.grid[x,y]==2:
-                    grido[x,y] = 1
-                gridnext[x,y] = self.next                
-                gridlast[x,y] = 0
+                    nn_input[0,x,y,1] = 1
+                #grid-next
+                nn_input[0,x,y,2] = self.next-1.0                
+                #grid-last
+                nn_input[0,x,y,3] = 0
         if self.nr>0:
-            gridlast[self.moves[self.nr-1,0], self.moves[self.nr-1,1]] = 1                 
+            nn_input[0,self.moves[self.nr-1,0], self.moves[self.nr-1,1],3] = 1                 
         
     def check_win(self,x,y,curr):
         #horizontal
