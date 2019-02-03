43b8c7604ecf7b6b6117d97ba5ffb53d5c65497b
Máté Bartalos <bartmate@gmail.com>
2019-02-03 21:28:07 +0100

    Preparing for 1st longer run

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 #Nr. of Simulation in MTCS
-MCTS_NR   = 128 #256    
+MCTS_NR   = 64 #256    
 
 #Temperature for controlling exploration
 MCTS_TAU  = 0.5
@@ -27,10 +27,10 @@ PIPELINE_HISTORY_SIZE     = 100000
 PIPELINE_BATCH_SIZE       = 32 #256
 
 # Evaluating the trained network after so many training steps
-PIPELINE_TRAINING_LOOP_NR = 100 #1000
+PIPELINE_TRAINING_LOOP_NR = 50 #1000
 
 # Playing so many games with the current best model to generate training data
-PIPELINE_SELFPLAY_NR      = 100 #100
+PIPELINE_SELFPLAY_NR      = 50 #100
 
 # Playing so many games to decide if the trained modell is better than the best-so-far model
 PIPELINE_EVAL_NR          = 40
